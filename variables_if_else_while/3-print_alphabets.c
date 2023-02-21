ef638b73331f5c1d8dedaa9ea9fbbfb410d104dd
Laurent81000 <6279@holbertonstudents.com>
2023-02-21 05:40:51 -0800

    Alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,24 +6,27 @@
 
 #include <stdio.h>
 
-int main(void) {
+int main(void) 
+{
     int i = 97;
 
-    /* Print lowercase letters */
-    while (i < 123) {
+    /*Print lowercase letters*/
+    while (i < 123) 
+    {
         putchar(i);
         i++;
     }
 
     i = 65;
 
-    /* Print uppercase letters */
-    while (i < 91) {
+    /*Print uppercase letters*/
+    while (i < 91) 
+    {
         putchar(i);
         i++;
     }
 
-    /* Print newline character */
+    /*Print newline character*/
     putchar('\n');
 
     return 0;
