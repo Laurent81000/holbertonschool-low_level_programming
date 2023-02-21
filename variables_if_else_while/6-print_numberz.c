4f62a66830b511dd7cd64701b17fb55dfb327098
Laurent81000 <6279@holbertonstudents.com>
2023-02-21 10:47:07 -0800

    10 starting from 0 with putchar without char

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+/**
+ *main - prints all single digit numbers of base 10 starting from 0
+ *
+ *Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+        int i = 0;
+
+        while (i < 10)
+        {
+                printf("%d", i);
+                i++;
+        }
+
+        printf("\n");
+        return (0);
+}
