be7088571cdaf7b9593a276d7f1f222a4955a009
Laurent81000 <6279@holbertonstudents.com>
2023-02-21 10:51:12 -0800

    10 starting from 0 with putchar without char

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,14 +8,25 @@
 
 int main(void)
 {
-        int i = 0;
+	int i = 0;
+	int j = 0;
 
-        while (i < 10)
-        {
-                printf("%d", i);
-                i++;
-        }
+	while (i < 10)
+	{
+	j = i + '0'; /*convertir l'entier en caractère numérique*/
+	putchar(j);
+	i++;
+	}
+	putchar('\n'); /*afficher un caractère de nouvelle ligne*/
 
-        printf("\n");
-        return (0);
+	i = 0;
+	while (i < 10)
+	{
+	j = i + 10 + '0'; /*convertir l'entier en caractère numérique*/
+	putchar(j);
+	i++;
+	}
+	putchar('\n'); /*afficher un caractère de nouvelle ligne*/
+
+	return (0);
 }
