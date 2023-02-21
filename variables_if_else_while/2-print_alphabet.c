fb0e0bcd8fd1fec632890ce84d80af9baf55cddd
Laurent81000 <6279@holbertonstudents.com>
2023-02-21 06:44:31 -0800

    Alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+/**
+ * main - Program that prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+        int i = 97;
+        /*Print lowercase letters*/
+        while (i < 123)
+        {
+                putchar(i);
+                i++;
+        }
+
+        i = 65;
+
+        /*Print newline character*/
+        putchar('\n');
+
+        return (0);
+}
