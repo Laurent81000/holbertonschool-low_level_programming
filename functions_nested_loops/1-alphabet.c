4fbf1c1a7ec524f0cd4f6c4eb624f69737e66717
Laurent81000 <6279@holbertonstudents.com>
2023-02-24 06:32:14 -0800

    alphabet prototype

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
+#include "main.h"
+
 /**
  * main - alphabet - prototype
  *
  * Return: Always 0 (Success)
  */
 
-#include "main.h"
-
-void print_alphabet()
+void print_alphabet(void)
 {
 	int i = 97;
 	/*Print lowercase letters*/
