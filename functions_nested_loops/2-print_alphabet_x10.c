890694100928c69153b1bf36349a94b89ab44c74
Laurent81000 <6279@holbertonstudents.com>
2023-02-24 07:12:10 -0800

    prints 10 times the alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - alphabet x10
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 97;
+	int x10 = 0;
+
+	while (x10 <= 10)
+
+	{
+		x10++;
+		/*Print lowercase letters*/
+		while (i < 123)
+		{
+			_putchar(i);
+			i++;
+		}
+
+		i = 65;
+
+		/*Print newline character*/
+		_putchar('\n');
+	}
+}
