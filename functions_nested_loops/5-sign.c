17a0ac4b97c22b52b630072a8b080fa883c5acbb
Laurent81000 <6279@holbertonstudents.com>
2023-02-25 07:11:24 -0800

    sign of a number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign -  prints the sign of a number
+ * Return - 1 is true, 0 is false 
+ */
+int print_sign(int n)
+{
+	int result;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		result = 1;
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		result = 0;
+	}
+
+	/* n < 0 */
+	else
+	{
+		_putchar('-');
+		result = -1;
+	}
+
+	return(result);
+}
