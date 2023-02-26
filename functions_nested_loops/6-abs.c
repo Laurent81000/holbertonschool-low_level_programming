aad6b9b804c1a2f6e3a63b38127dad978c760ba8
Laurent81000 <6279@holbertonstudents.com>
2023-02-26 10:49:14 -0800

    absolute value

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * int _abs -  absolute value
- * @aNumbers: the number to check
+ * @_abs: the number to check
  *
  * Description: absolute value is always positive
  * Return: aNumber is the absolute value
