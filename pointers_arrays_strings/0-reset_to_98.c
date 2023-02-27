5903b2c599251d37bdbcbd22eb9b99bab5198b0e
Laurent81000 <6279@holbertonstudents.com>
2023-02-27 01:22:40 -0800

    README

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  function
+ * @n: the caracter to check
+ *
+ * Description: takes a pointer to an int
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+
+return (0);
+}
