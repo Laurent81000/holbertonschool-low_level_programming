e7346e5412f3f20f2e5d0d5677259b7233cd6a7b
Laurent81000 <6279@holbertonstudents.com>
2023-02-27 09:00:29 -0800

    prints a string to stdout

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - Affiche une chaîne de caractères sur stdout, suivie d'un saut de ligne.
+ *
+ * @str: pointeur vers la chaîne de caractères à afficher.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	/* parcourir chaque caractère de la chaîne */
+	while (str[i] != '\0') 
+	{
+		/* afficher le caractère courant sur stdout */
+		_putchar(str[i]);
+		i++;
+	}
+	/* afficher un caractère de saut de ligne à la fin de la chaîne */
+	_putchar('\n');
+}
