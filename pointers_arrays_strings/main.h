9c8c5a606e29759acf01be47ac8d074f03ae06a5
Laurent81000 <6279@holbertonstudents.com>
2023-02-28 05:04:19 -0800

    prints every other character of a string

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-int _putchar(char c);
 void print_rev(char *s);
+void puts2(char *str);
