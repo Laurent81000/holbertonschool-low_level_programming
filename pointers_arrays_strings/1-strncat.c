2a99f0c89391a4ce775746b8641413304eeaaea1
Laurent81000 <6279@holbertonstudents.com>
2023-03-02 06:07:31 -0800

    concatenates two strings maximum number of bytes

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _strncat - concatenates two strings.
  * @dest: destination string.
@@ -11,12 +13,15 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != '\0'; i++);
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
 		dest[i + j] = src[j];
 
-	dest[i + j] = '\0';
-
+		dest[i + j] = '\0';
+	}
 	return (dest);
 }
