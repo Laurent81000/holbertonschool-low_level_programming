2cb633874c1c2225d491ccb4c4115cad3c1c4777
Laurent81000 <6279@holbertonstudents.com>
2023-03-03 14:16:18 -0800

    locates a substring - GPT

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrence of the substring needle
+ * in the string haystack
+ * @haystack: the string to be searched
+ * @needle: the substring to be found
+ *
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack) {
+		char *beginning = haystack;
+		char *pattern = needle;
+		while (*haystack && *pattern && *haystack == *pattern) {
+			haystack++;
+			pattern++;
+		}
+		if (!*pattern) {
+			return beginning;
+		}
+		haystack = beginning + 1;
+	}
+	return NULL;
+}
