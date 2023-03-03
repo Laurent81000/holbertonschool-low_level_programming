3542da8868a664d87981b00e24a8ff02b67a5a14
Laurent81000 <6279@holbertonstudents.com>
2023-03-03 14:49:14 -0800

    copies memory area

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer to the destination buffer
+ * @src: pointer to the source buffer
+ * @n: number of bytes to copy
+ *
+ * Return: a pointer to the destination buffer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
