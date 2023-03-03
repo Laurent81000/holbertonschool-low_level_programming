e5ace6632c497daa0188e57a1ca4fd37bf3304f0
Laurent81000 <6279@holbertonstudents.com>
2023-03-03 12:59:21 -0800

    gets the length of a prefix substring - GPT

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ *
+ * @s: The string to be searched
+ * @accept: The characters to search for
+ *
+ * Return: The number of bytes in the initial segment of s
+ *         which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	char *p;
+
+	while (*s)
+	{
+		for (p = accept; *p; p++)
+		{
+			if (*s == *p)
+			{
+				count++;
+				break;
+			}
+		}
+		if (!*p)
+			break;
+		s++;
+	}
+
+	return count;
+}
