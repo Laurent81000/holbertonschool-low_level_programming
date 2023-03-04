27e90cbe9263d900af1f095de1c0405084059030
Laurent81000 <6279@holbertonstudents.com>
2023-03-04 04:50:17 -0800

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - self-explanatory
+ *
+ * Return: always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+
+		else
+		{
+			if (i % 3 == 0)
+				printf("Fizz");
+
+			if (i % 5 == 0)
+				printf("Buzz");
+		}
+
+		printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
