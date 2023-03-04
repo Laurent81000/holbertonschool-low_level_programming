735ecae0d3009f28d7fcf5335a9df8c6e57a9588
Laurent81000 <6279@holbertonstudents.com>
2023-03-04 01:30:45 -0800

    checks for uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - Determine the upperness of a character
+ * @c: character to test
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
