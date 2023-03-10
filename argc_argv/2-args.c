24e74385d69531a2bd78668b36790eb6ad817072
Laurent81000 <6279@holbertonstudents.com>
2023-03-10 09:56:56 -0800

    prints all arguments

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
- * main - Entry point. Prints all arguments it receives
- * @argc: Number of arguments passed to the program
- * @argv: Array of strings containing the arguments passed to the program
+ * main - entry point
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
  *
- * Return: Always 0
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, length = 0;
 
-	/* Print the name of the script */
-	printf("%s\n", argv[0]);
-
-	/* Print all other arguments */
 	for (i = 1; i < argc; i++)
 	{
-		printf("%s", argv[i]);
-
-		if (i != argc - 1)
-			printf("\n");
+		printf("%s\n", argv[i]);
+		/* add 1 for the newline character */
+		length += strlen(argv[i]) + 1;
 	}
+
+	/* print the program name as the first line of output */
+	printf("%s\n", argv[0]);
+
+	/* print the length of the output (not including the program name) */
+	fprintf(stderr, "%d chars long\n", length);
 
 	return (0);
 }
