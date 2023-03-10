121e53a2b2f613269c4d524c660b8218269ecfb4
Laurent81000 <6279@holbertonstudents.com>
2023-03-10 10:21:38 -0800

    prime number - PLD - GPT

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -5,19 +5,34 @@
  *
  * Return: 1 if the number is prime, 0 otherwise
  */
+int is_prime_helper(int n, int i);
+
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1) /* 1 and below are not prime */
 		return (0);
 
-	/* Check if n is divisible by any number from 2 to n-1 */
-	for (i = 2; i < n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
+	return is_prime_helper(n, 2);
+}
 
-	return (1);
+/**
+ * is_prime_helper - recursive helper function to check if a number is prime
+ *
+ * @n: the number to check
+ * @i: the current divisor to check
+ *
+ * Return: 1 if the number is prime, 0 otherwise
+ */
+int is_prime_helper(int n, int i)
+{
+	/* Base case: n is divisible by i, so it's not prime */
+	if (n % i == 0)
+		return (0);
+
+	/* Base case: we've reached the end of the loop, so it's prime */
+	if (i * i > n)
+		return (1);
+
+	/* Recursive case: check the next divisor */
+	return is_prime_helper(n, i + 1);
 }
