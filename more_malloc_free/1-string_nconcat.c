c411c3a15f653b5119c45b54ba06264a16e44c0d
Laurent81000 <6279@holbertonstudents.com>
2023-03-17 05:48:46 -0700

    concatenates two strings + malloc

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to copy from s2
+ * Return: pointer to the new allocated space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int s1_len; // Défini longeur chaine 1 connue
+	unsigned int i; // Compteur
+	unsigned int o = n; // Permet d'utiliser n
+	char* str; //Nouvelle chaine de caractères
+
+	for (s1_len = 0; s1[s1_len]; s1_len++) //Parcours s1 et compte nb caractères 
+		o++; // Variable o incrémentée pour refléter ce nb dans str
+
+	str = malloc((o + 1) * sizeof(char)); //Alloue de la mémoire à la nouvelle chaine str
+	if (str == NULL)	//Syntaxe de malloc, STOP si NULL ou 0
+		return (NULL);
+
+	o = 0;	//Iiniter o
+
+	for (i = 0; s1[i]; i++)	//Boucle pour copier s1 dans str
+		str[o++] = s1[i]; 
+
+	for (i = 0; s2[i] && i < n; i++) // Boucle pour copier s2 dans str
+		str[o++] = s2[i];	
+
+	str[o] = '\0'; //Jusqu'à \0 car caractères (char) 
+
+	return (str); 
+}
+
+int main(void)
+{
+	char *concat;
+
+	concat = string_nconcat("Best ", "School !!!", 5);
+	printf("%s\n", concat);
+	free(concat);
+	return (0);
+}
+
