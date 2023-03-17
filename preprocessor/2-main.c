f928e180c93b1632ae1930620de5a42225509cd4
Laurent81000 <6279@holbertonstudents.com>
2023-03-17 13:20:19 -0700

    Préprocessor - program that prints the name of the file

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main: Function that return nothing
+ * Return: 0
+ */
+
 int main(void)
 {
-	printf("%s\n", __FILE__); /* Preprocessor - Prints the name of the file + new line */	
+	printf("%s\n", __FILE__); 
+/* Preprocessor - Prints the name
+ * of the file + new line
+ */
 	return (0);
 }
