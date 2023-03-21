885ffe9442acd388ff08f91cf2116ec1595ca4a2
Laurent81000 <6279@holbertonstudents.com>
2023-03-21 06:58:55 -0700

    function - creates a new dog - malloc - struct - 2 codes

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -60,3 +60,53 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (dog);
 }
+
+
+OU
+
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *copy_name, *copy_owner;
+	int i;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	copy_name = malloc(sizeof(name));
+	if (copy_name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	for (i = 0; name[i]; i++)
+		copy_name[i] = name[i];
+	copy_name[i] = '\0';
+
+	copy_owner = malloc(sizeof(owner));
+	if (copy_owner == NULL)
+	{
+		free(new_dog);
+		free(copy_name);
+		return (NULL);
+	}
+
+	for (i = 0; owner[i]; i++)
+		copy_owner[i] = owner[i];
+	copy_owner[i] = '\0';
+
+	init_dog(new_dog, copy_name, age, copy_owner);
+
+	return (new_dog);
+}
